bc6a1cea93c5e5e5767d48e7b6f93d4613d8bbd7
phosu7 <99696023+phosu7@users.noreply.github.com>
2023-10-09 01:37:33 -0500

    creation of tutorial scene

---
+++ GDFinalProject/Assets/MainMenuHandler.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuHandler : MonoBehaviour
+{
+    public void PlayGame(){
+       
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+}
