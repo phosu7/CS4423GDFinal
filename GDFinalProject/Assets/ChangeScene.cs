2d8c6707147d701d9112dfb51d380f6c5f92b1c3
phosu7 <99696023+phosu7@users.noreply.github.com>
2023-10-17 19:43:21 -0500

    Dynamic SceneChanger

--- GDFinalProject/Assets/ChangeScene.cs
+++ GDFinalProject/Assets/ChangeScene.cs
@@ -5,7 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+    [SerializeField] string nextScene;
     void OnTriggerEnter2D(Collider2D other){
-        SceneManager.LoadScene("TitleScreen");
+        SceneManager.LoadScene(nextScene);
     }
 }
