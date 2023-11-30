9e3a2bdb12f67b346a404738d1097859d36bb435
phosu7 <99696023+phosu7@users.noreply.github.com>
2023-11-30 16:28:17 -0600

    particle system

--- GDFinalProject/Assets/DeathCounter.cs
+++ GDFinalProject/Assets/DeathCounter.cs
@@ -1,12 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DeathCounter : MonoBehaviour
 {
     public int deaths = 0;
     public static DeathCounter singleton;
+    int counter = 1;
+    void Update(){
+        string currentScene = SceneManager.GetActiveScene().name;
+            if (currentScene=="FirstStage" && counter==1){  //tutorial stage reset death counter
+                deaths=0;
+                counter++;
+            }
+            if (currentScene=="TitleScreen" && counter >= 1){ //after beating the game it sends to title screen and resets death counter
+                deaths=0;
+                counter++;
+            }
 
+    }
     void Awake(){
         if (singleton == null){
             singleton = this;
--- GDFinalProject/Assets/PointsCounter.cs
+++ GDFinalProject/Assets/PointsCounter.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PointsCounter : MonoBehaviour
 {
     public int points = 0;
     public static PointsCounter singleton;
+    
+    void Update(){
+        string currentScene = SceneManager.GetActiveScene().name;
+            if (currentScene=="TitleScreen"){ //after beating the game it sends to title screen and resets death counter
+                points=0;
+            }
 
+    }
     void Awake(){
         if (singleton == null){
             singleton = this;
